35ec581e992f88b126d51fbdb25496fed862ae10
SariMelina <mmkings_beba@hotmail.com>
2021-01-22 12:58:45 -0600

    'Add a task' as a component

--- vue.js
+++ vue.js
@@ -1,26 +1,62 @@
+const data = {
+    tareas: [
+        {
+            texto : 'Aprender Vue.js',
+            terminada: false
+        },
+        {
+            texto : 'Aprender Angular 2',
+            terminada: false
+        },
+        {
+            texto : 'Aprender Ionic 2',
+            terminada: false
+        },
+        
+    ],
+    nuevaTarea: ''
+}
+
+Vue.component('nueva-tarea', {
+    template: `<div class="input-group">
+    <input type="text" placeholder="Escribe una nueva tarea..." 
+    v-model="nuevaTarea"
+    class="form-control"
+    @keyup.enter="agregartarea()">
+    <span class="input-group-btn">
+        <button type="button" @click="agregartarea()"
+        class="btn btn-primary">Agregar</button>
+    </span>
+</div>`,
+data: function(){
+    return data;
+},
+methods: {
+    agregartarea: function(){
+        const tarea = this.nuevaTarea.trim()
+        if(tarea){
+            this.tareas.push({
+                texto: tarea,
+                terminada: false
+            })
+        }            
+        this.nuevaTarea = ''
+    }
+}
+})
+
+Vue.component('titulo', {
+    template:'<h2>{{titulo}}</h2>',
+    data: function(){
+        return { titulo : '#Lista de Tareas'}
+    }
+});
+
 const app = new Vue({
     el: '#app',
-    data: {
-        titulo: 'Lista de Tareas',
-        tareas: [
-            {
-                texto : 'Aprender Vue.js',
-                terminada: false
-            },
-            {
-                texto : 'Aprender Angular 2',
-                terminada: false
-            },
-            {
-                texto : 'Aprender Ionic 2',
-                terminada: false
-            },
-            
-        ],
-        nuevaTarea: ''
-    },
+    data: data,
     methods: {
-        agregartarea : function(){
+/*         agregartarea : function(){
             const tarea = this.nuevaTarea.trim()
             if(tarea){
                 this.tareas.push({
@@ -29,7 +65,7 @@ const app = new Vue({
                 })
             }            
             this.nuevaTarea = ''
-        },
+        }, */
         borrarTarea: function(index){
             this.tareas.splice(index,1)
         }
